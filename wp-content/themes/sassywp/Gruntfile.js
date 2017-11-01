3a4e592fa8768f687654b9da1e0f96c8c76d3ceb
thisispayam <thisispyam@gmail.com>
2017-10-31 23:01:01 -0400

    starter

---
+++ wp-content/themes/sassywp/Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+       
+        pkg: grunt.file.readJSON('package.json'), 
+        
+         /** Sass task **/
+        
+        sass: {
+            dev: {
+                options: {
+                  style: 'expanded',
+                    sourcemap: 'none',
+                },
+                files: {
+                    'compiled/style-human.css':'sass/style.scss'
+                }
+            },
+          
+            dist: {
+                options: {
+                  style: 'compressed',
+                    sourcemap: 'none',
+                },
+                files: {
+                    'compiled/style.css':'sass/style.scss'
+                }
+            }
+        },
+        
+          
+            /** Autoprefixer **/
+            autoprefixer: {
+                options: {
+                    browsers: ['last 2 versions']
+                },
+                //prefix all files
+                multiple_files: {
+                    expand: true,
+                    flatten: true,
+                    src: 'compiled/*.css',
+                    dest: ''
+                }
+            },
+            
+        
+        /** Watch task **/
+        
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass','autoprefixer']
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.registerTask('default', ['watch']);
+}
---
+++ wp-content/themes/sassywp/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package sassy-wp
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sassywp' ) ); ?>"><?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'sassywp' ), 'WordPress' );
+			?></a>
+			<span class="sep"> | </span>
+			<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'sassywp' ), 'sassywp', '<a href="http://payam.ca">Payam Kazemi</a>' );
+			?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
